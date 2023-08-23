a0b6ffb1124dbb7aae3bb7636507978844eefd4d
devalibello <alibelloimoukhuede@gmail.com>
2023-08-23 23:43:05 +0100

    Fix linters error

--- classes/author.rb
+++ classes/author.rb
@@ -1,15 +1,15 @@
 class Author
-    attr_accessor :first_name, :last_name
-    attr_reader :items
-  
-    def initialize(first_name, last_name)
-      @first_name, @last_name = first_name, last_name
-      @items = []
-    end
-  
-    def add_item(item)
-      @items << item unless @items.include?(item)
-      item.author = self
-    end
+  attr_accessor :first_name, :last_name
+  attr_reader :items
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
   end
-  
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.author = self
+  end
+end
