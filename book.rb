60d620d31807cf215183fad516ffa4cfdeda02b4
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-23 12:32:33 -0400

    Update book.rb file

--- book.rb
+++ book.rb
@@ -1,13 +1,17 @@
 require_relative 'item'
+require_relative 'author'
+require_relative 'genre'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state, :label
+  attr_accessor :publisher, :cover_state, :label, :author, :genre
 
-  def initialize(id, publish_date, publisher, cover_state, archived: false)
+  def initialize(id, publish_date, publisher, cover_state, author: nil, genre: nil, archived: false)
     super(id, publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
     @label = nil
+    @author = author
+    @genre = genre
   end
 
   def can_be_archived?
@@ -16,8 +20,10 @@ class Book < Item
 
   def self.list_books(books)
     books.each do |book|
+      author_name = book.author ? "#{book.author.first_name} #{book.author.last_name}" : 'N/A'
+      genre_name = book.genre ? book.genre.name : 'N/A'
       label_title = book.label ? book.label.title : 'N/A'
-      puts "Book ID: #{book.id}, Title: #{label_title}, Publisher: #{book.publisher}"
+      puts "Book ID: #{book.id}, Title: #{label_title}, Publisher: #{book.publisher}, Author: #{author_name}, Genre: #{genre_name}"
     end
   end
 end
