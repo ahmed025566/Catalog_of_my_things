f627de34b23aeb10672253ed568b7f19bd9b0703
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-24 00:05:36 -0400

    Update schema.sql file

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,36 @@
-CREATE TABLE author(
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL
+-- books table
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  publisher VARCHAR(255) NOT NULL,
+  cover_state VARCHAR(20) NOT NULL,
+  archived BOOLEAN DEFAULT false,
+  author_id INTEGER,
+  genre_id INTEGER,
+  label_id INTEGER,
+  FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE SET NULL,
+  FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE SET NULL,
+  FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE SET NULL
+);
+
+-- labels table
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  color VARCHAR(50) NOT NULL
+);
+
+-- authors table
+CREATE TABLE authors (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL
+);
+
+-- genres table
+CREATE TABLE genres (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
 );
 
 -- Games Table
