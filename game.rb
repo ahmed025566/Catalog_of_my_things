5a157727b83846001dc9c7d6e75635724350df31
ahmed025566 <ahmedsalah025566@gmail.com>
2023-08-24 07:24:09 +0300

    Fix bugs

--- game.rb
+++ game.rb
@@ -1,10 +1,11 @@
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(multiplayer, last_played_at, publish_date)
-    super(publish_date)
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(id, publish_date, multiplayer, last_played_at , archived: false)
+    super(id, publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
